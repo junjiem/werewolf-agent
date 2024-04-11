60fa0891356c856cd554646c6a2d69ba0aeb73f4
junjie.miao <junjie.miao@goupwith.com>
2024-04-11 20:13:00 +0800

    update

--- src/main/java/me/junjiem/werewolf/agent/util/ChatLanguageModelUtil.java
+++ src/main/java/me/junjiem/werewolf/agent/util/ChatLanguageModelUtil.java
@@ -6,6 +6,7 @@ import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.dashscope.QwenChatModel;
 import dev.langchain4j.model.dashscope.QwenModelName;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import me.junjiem.werewolf.agent.bean.SpeakResult;
 
 import java.util.Optional;
@@ -16,6 +17,7 @@ import java.util.regex.Pattern;
  * @Author JunjieM
  * @Date 2024/4/9
  */
+@Slf4j
 public class ChatLanguageModelUtil {
 
     private static final Pattern JSON_PATTERN = Pattern
@@ -39,10 +41,13 @@ public class ChatLanguageModelUtil {
     public static <T> T jsonAnswer2Object(String answer, Class<T> classOfT) {
         Matcher matcher = JSON_PATTERN.matcher(answer);
         if (matcher.find()) {
+            log.debug("Code Block Type: " + matcher.group(1));
             answer = matcher.group(2);
         }
         Gson gson = new Gson();
-        return gson.fromJson(answer.replaceAll(",\\s*}", "}"), classOfT);
+        answer = answer.replaceAll(",\\s*}", "}");
+        log.info("JSON: " + answer);
+        return gson.fromJson(answer, classOfT);
     }
 
 }
